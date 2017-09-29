d6c41cd52251eda03a67f9b667a5954fad7e4510
GusVilla303 <gusvilla303@gmail.com>
2017-09-29 17:50:13 -0600

    Add #print_stories to CLI. Need to improve output for readibility.

--- lib/npr_todays_news/cli.rb
+++ lib/npr_todays_news/cli.rb
@@ -5,8 +5,17 @@ class NprTodaysNews::CLI
 
   def get_stories
     puts "These are today's top stories on NPR:"
-    NprTodaysNews::Scraper.new.scrape
+    news_list = NprTodaysNews::Scraper.new.scrape
+    print_stories(news_list)
   end
+
+  def print_stories(news_list)
+    news_list.stories.each.with_index(1) do |story, index|
+      puts "#{index}. #{story.title} -- #{story.teaser}"
+    end
+  end
+
+
   # this is where I want to receive input from the user to loop through his/her options.
 
 
