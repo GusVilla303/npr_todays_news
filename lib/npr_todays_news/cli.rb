6accc8a8d5e852210df50f6fac207d80a4187f28
GusVilla303 <gusvilla303@gmail.com>
2017-10-03 14:43:40 -0600

    Improve output readibility for featured stories.

--- lib/npr_todays_news/cli.rb
+++ lib/npr_todays_news/cli.rb
@@ -1,23 +1,33 @@
 class NprTodaysNews::CLI
   def run
-    get_newslist
     print_stories
   end
 
   def get_newslist
     puts "These are today's top stories on NPR:"
+    puts
     NprTodaysNews::Scraper.new.scrape
   end
 
   def print_stories
     get_newslist.stories.each.with_index(1) do |story, index|
-      puts "#{index}. #{story.title} -- #{story.teaser}"
+      puts "#{index}. #{story.title}"  #-- #{story.teaser}
+      #binding.pry
     end
+    puts
+    get_user_input
   end
 
+  def get_user_input
+    puts "Please select the story number (#1-3) or type 'exit' to quit."
+    input = gets.strip.to_i
 
-  # this is where I want to receive input from the user to loop through his/her options.
+    if input == 1
+      puts "you chose the first story!"
+    end
 
+    #
+  end
 
 
 end
