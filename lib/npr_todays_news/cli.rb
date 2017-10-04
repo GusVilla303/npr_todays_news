020ca6f3c287442fcdf1532c754fbcf00f6b5d66
GusVilla303 <gusvilla303@gmail.com>
2017-10-03 22:27:13 -0600

    Add CLI loop to list stories, get user input, and render the selected story or exit loop.

--- lib/npr_todays_news/cli.rb
+++ lib/npr_todays_news/cli.rb
@@ -1,32 +1,44 @@
 class NprTodaysNews::CLI
 
-  @@scraper = ""
+  def initialize
+    puts "Please type 'list' to list feature stories or 'exit' to quit."
+
+    scraper = NprTodaysNews::Scraper.new
+    @news_list = scraper.scrape_featured_stories
+  end
 
   def run
-    print_stories
+    input = ""
+    while input != "exit"
+      puts
+      input = gets.strip
+
+      case input
+      when "list"
+        puts "Here are today's featured stories:"
+        puts
+        list_stories
+      when "exit"
+        puts "Goodbye!"
+        exit
+      else
+        system("open #{@news_list.stories[input.to_i-1].url}")
+        options_output
+      end
+    end
   end
 
-  def get_newslist
-    puts "These are today's top stories on NPR:"
-    puts
-    @scraper = NprTodaysNews::Scraper.new
-    @scraper.scrape_featured_stories
-  end
-
-  def print_stories
-    get_newslist.stories.each.with_index(1) do |story, index|
+  def list_stories
+    @news_list.stories.each.with_index(1) do |story, index|
       puts "#{index}. #{story.title}"  #-- #{story.teaser}
     end
-    puts ""
-    puts "Please select the story number (#1-3) or type 'exit' to quit."
-    input = gets.strip.to_i
-
-    if input == 1
-      url = story.url
-      @scraper
-    end
+    options_output
   end
 
+  def options_output
+    puts ""
+    puts "To read a story in your browser, type (#1-3) or 'exit' to quit."
+  end
   # def get_user_input
   #   puts "Please select the story number (#1-3) or type 'exit' to quit."
   #   input = gets.strip.to_i
